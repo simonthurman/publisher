efedb4976cfac675b6104f947b0d938ff24766a9
Simon Thurman <simont@microsoft.com>
2021-08-16 16:24:54 +0100

    initial

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using Azure.Messaging.WebPubSub;
 
 namespace publisher
 {
@@ -6,7 +8,17 @@ namespace publisher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length != 3) {
+                Console.WriteLine("Usage: publisher <connectionString> <hub> <message>");
+                return;
+            }
+            var connectionString = args[0];
+            var hub = args[1];
+            var message = args[2];
+            
+            // Either generate the token or fetch it from server or fetch a temp one from the portal
+            var serviceClient = new WebPubSubServiceClient(connectionString, hub);
+            serviceClient.SendToAll(message);
         }
     }
 }
